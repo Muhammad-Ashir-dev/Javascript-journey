e1c6fac31bf901b1c175a95612c51b4d81085d35
Muhammad Ashir <ashirrajpoot514@gmail.com>
2026-03-13 12:40:10 +0500

    Data Types complete - Stage 01

--- hello.js
+++ hello.js
@@ -2,3 +2,78 @@
 console.log("Hey its me Ashir learning Js");
 //learning Date
 console.log("Day of learning 12/03/2026 ");
+
+
+//Variables
+//var  //let  //const
+
+//const name = "Muhammad Ashir";
+//let age = 19;
+//let city = "Rawalpindi";
+
+//console.log(name);
+//console.log(age);
+//console.log(city);
+
+//7 Data types
+//String
+//Number
+//Bolean
+//Bigint
+//null
+//Undefined
+//Symbol
+
+
+//string
+//let name = "Muhammad Ashir";
+//let age = 19;
+//let hobby = "Cricket";
+
+//console.log(name);
+//console.log(age);
+//console.log(hobby);
+
+//number
+//let age = 19;
+//let score = 300;
+//let price = 100000;
+
+//let nextYear = age + 1;
+//
+//console.log(age);
+//console.log(score);
+//console.log(price);
+//console.log(nextYear);
+
+
+//Boolean
+
+//let isStudent = true;
+//let isMarried = false;
+//let isPakistani = true;
+
+//console.log(isStudent);
+//console.log(isMarried);
+//console.log(isPakistani);
+
+//Profile practice
+let name = "Muhammad Ashir";
+let age = 19;
+let city = "Rawalpindi";
+let isStudent = true;
+let hasJob = false;
+let score = 250;
+let girlFriend = null;
+let paidInternship = null;
+let future;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof isStudent);
+console.log(typeof hasJob);
+console.log(typeof score);
+console.log(typeof girlFriend);
+console.log(typeof paidInternship);
+console.log(typeof future);
